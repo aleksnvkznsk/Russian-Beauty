691aee59818fece74f419f021026db765c86d31d
aleksnvkznsk <aleksandrov.developer@gmail.com>
2025-01-20 17:15:05 +0700

    create toggleMenu

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,7 @@
+"use client"
 import Link from "next/link";
 import "./header.scss";
+import { useState } from "react";
 
 const Header = () => {
     const navLinks = [
@@ -11,20 +13,30 @@ const Header = () => {
         { href: "/contacts", label: "Контакты" },
     ];
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header className="header">
             <p className="header__logo">Russian Beauty</p>
-            <nav className="header__nav">
-                <ul className="header__links">
-                    {navLinks.map((link, index) => (
-                        <li key={index} className="header__links__item">
-                            <Link href={link.href} className="header__links__link">
-                                {link.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+            <div className="header__nav-container">
+                <button className="header__burger" onClick={toggleMenu}>☰</button>
+                <nav className={`header__nav ${isMenuOpen ? 'header__nav--open' : ''}`}>
+                    <div className="header__nav-content">
+                        <button className="header__nav-close" onClick={toggleMenu}>X</button>
+                        <ul className="header__links">
+                            {navLinks.map((link, index) => (
+                                <li key={index} className="header__links__item">
+                                    <Link href={link.href} className="header__links__link" onClick={toggleMenu}>{link.label}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </nav>
+            </div>
             <p className="header__phone">+7 999 888 77 66</p>
         </header>
     );
