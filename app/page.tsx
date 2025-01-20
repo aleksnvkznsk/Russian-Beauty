18daa3da43ebf83fd76a33bf21447229dee4551e
aleksnvkznsk <aleksandrov.developer@gmail.com>
2025-01-20 17:45:11 +0700

    add components to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Doctors from "@/components/doctors/doctors";
+import FAQ from "@/components/faq/faq";
 
 export default function Home() {
   return (
     <>
+      <Doctors />
+      <FAQ />
     </>
   );
 }
